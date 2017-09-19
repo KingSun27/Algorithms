70dedebb247a84485ed53a4e8572671084d32da9
KingSun27 <270403579@qq.com>
2017-09-19 11:38:19 +0800

    @first commit

---
+++ src/main/java/sort/basic/BasicSort.java
@@ -0,0 +1,46 @@
+package sort.basic;
+
+import utils.StdRandom;
+
+/**
+ * �����㷨��һЩ����������
+ * ��Ƚϣ��������Ƿ��������������
+ *
+ */
+public class BasicSort {
+	public static  void sort(Comparable[] a){}
+	public static boolean less(Comparable a,Comparable b){
+		return a.compareTo(b)<0;
+	}
+	public static void exch(Comparable[] a, int i, int j){
+		Comparable temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	/**
+	 * �������������Ƿ��С��������
+	 */
+	public static boolean isSorted(Comparable[] a){
+		for (int i = 1; i < a.length; i++) {
+			if(less(a[i],a[i-1]))  return false;
+		}
+		return true;
+	}
+	public static void show(Comparable[] a){
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();
+	}
+	public static void run() {
+		int size = 10;
+		Double[] a = new Double[size];
+		for (int j = 0; j < size; j++) {
+			a[j] =StdRandom.uniform();
+		}
+		sort(a);
+		System.out.println(isSorted(a));
+		show(a);
+	}
+
+}
